e04c7b851bdc830c08223ccc2bb474c22c68863e
Maxim Merkulov <m.merkulovdodo@gmail.com>
2026-08-12 15:29:17 +0300

    Fix api import path for Docker

--- src/event_aggregator/main.py
+++ src/event_aggregator/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from api.health import router as health_router
+from event_aggregator.api.health import router as health_router
 
 app = FastAPI()
 app.include_router(health_router)
